a8abc92d24538bb3df1968aa29b74b1afb190a04
Pedro Tebexreni <pedro.tebexreni@estudiono.com>
2026-02-13 17:52:05 -0300

    Feat: apredendo sobre dialog

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { ChecklistsWrapper } from "./components/ChecklistsWrapper"
 import { Container } from "./components/Container"
+import { Dialog } from "./components/Dialog"
 import { FabButton } from "./components/FabButton"
 import { Footer } from "./components/Footer"
 import { Header } from "./components/Header"
@@ -8,6 +9,7 @@ import { IconPlus, IconSchool } from "./components/icons"
 import { SubHeading } from "./components/SubHeading"
 import { ToDoItem } from "./components/ToDoItem"
 import { ToDoList } from "./components/ToDoList"
+export { Dialog } from "./components/Dialog"
 
 const todos = [
   {
@@ -60,6 +62,7 @@ function App() {
             <IconSchool /> Plano de estudos
           </Heading>
         </Header>
+        <Dialog></Dialog>
         <ChecklistsWrapper>
           <SubHeading>Para estudar</SubHeading>
           <ToDoList>
@@ -80,6 +83,7 @@ function App() {
           </Footer>
         </ChecklistsWrapper>
       </Container>
+      
     </main>
   )
 }
---
+++ src/components/Dialog/index.jsx
@@ -0,0 +1,25 @@
+import './dialog.style.css'
+
+export function Dialog() {
+    const dialog = document.querySelector("dialog");
+
+
+    // "Show the dialog" button opens the dialog modally
+    const openDialog = () => {
+        dialog.showModal();
+    };
+
+    // "Close" button closes the dialog
+    const closeDialog = () => {
+        dialog.close();
+    };
+    return (
+        <>
+            <dialog>
+                <button autoFocus onClick={closeDialog}>Close</button>
+                <p>This modal dialog has a groovy backdrop!</p>
+            </dialog>
+            <button onClick={openDialog}>Show the dialog</button>
+        </>
+    )
+}
